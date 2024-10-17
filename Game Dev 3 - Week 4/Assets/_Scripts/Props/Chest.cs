58c5f11bda4f6fbae014488289a69cf2247f5e40
Brooks <O.Brooks5757@student.leedsbeckett.ac.uk>
2024-10-17 10:41:23 +0100

    Interface Implemented

---
+++ Game Dev 3 - Week 4/Assets/_Scripts/Props/Chest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameDevWithMarco.EnvironmentalProps
+{
+    public class Chest : MonoBehaviour
+    {
+        Animator anim;
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void OpenTheChest()
+        {            
+            anim.SetTrigger("Open");
+        }
+
+        public void Interact()
+        {
+            OpenTheChest();
+        }
+    }
+}
